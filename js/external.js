e8b506d1c0d9d4834d21e59a28f713a371438cac
Jeffrey Neal <jeffneal11@gmail.com>
2022-11-21 11:27:07 -0600

    Completed js with html exercise

---
+++ js/external.js
@@ -0,0 +1,11 @@
+
+
+function exercise() {
+    console.log("Hello from external JavaScript");
+
+    alert("Welcome to my Website!");
+
+    alert("Great, " + prompt("What color do you like?") + " is my favorite color too.");
+}
+
+exercise();
